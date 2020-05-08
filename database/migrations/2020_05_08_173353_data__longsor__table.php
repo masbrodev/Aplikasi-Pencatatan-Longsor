88dcc44471ff939335c1e2b3af35d37bec842d66
Masbro <s2gotosingapore@gmail.com>
2020-05-09 02:02:02 +0700

    Database Longsor

---
+++ database/migrations/2020_05_08_173353_data__longsor__table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class DataLongsorTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('data_longsor', function (Blueprint $table) {
+            $table->Increments('id');
+            $table->string('desa');
+            $table->string('kecamatan');
+            $table->integer('jumlah_kejadian');
+            $table->integer('tahun');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
