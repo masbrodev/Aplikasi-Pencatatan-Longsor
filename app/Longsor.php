4adbcfeb02c6a762066718e09749c42bda6232d0
Masbro <s2gotosingapore@gmail.com>
2020-05-09 02:01:44 +0700

    Modal Longsor

---
+++ app/Longsor.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Longsor extends Model
+{
+    protected $table = 'data_longsor';
+    protected $primaryKey = 'id';
+    protected $fillable = ['desa', 'kecamatan', 'jumlah_kejadian', 'tahun'];
+}
