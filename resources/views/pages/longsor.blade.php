c1c0132923369b914587ac3876f7e8e890b76a52
Masbro <s2gotosingapore@gmail.com>
2020-05-09 02:05:15 +0700

    Modal Edit

--- resources/views/pages/longsor.blade.php
+++ resources/views/pages/longsor.blade.php
@@ -101,4 +101,54 @@
     <!-- /.modal-dialog -->
 </div>
 <!-- /.modal -->
+
+@php
+$u = 1;
+@endphp
+@foreach($longsor as $r)
+<div class="modal fade" id="edit-longsor-modal{{$u}}">
+    <div class="modal-dialog modal-lg">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h4 class="modal-title">Edit Data Longsor</h4>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+            <div class="modal-body">
+                <form action="{{ URL::to('longsor/edit/'.$r->id)}}" method="post">
+                    @csrf
+
+                    <div class="form-group">
+                        <label>Desa</label>
+                        <input type="text" class="form-control" placeholder="Masukkan Desa" name="desa" value="{{ $r->desa }}">
+                    </div>
+                    <div class="form-group">
+                        <label>Kecamatan</label>
+                        <input type="text" class="form-control" placeholder="Masukkan Kecamatan" name="kec" value="{{ $r->kecamatan }}">
+                    </div>
+                    <div class="form-group">
+                        <label>Jumha Kejadian</label>
+                        <input type="text" class="form-control" placeholder="Masukkan Jumha Kejadian" name="jumk" value="{{ $r->jumlah_kejadian }}">
+                    </div>
+                    <div class="form-group">
+                        <label>Tahun</label>
+                        <input type="text" class="form-control" placeholder="Masukkan Tahun" name="thn" value="{{ $r->tahun }}">
+                    </div>
+
+                    <div class="modal-footer justify-content-between">
+                        <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+                        <button type="submit" class="btn btn-primary">Simpan</button>
+                    </div>
+                </form>
+
+            </div>
+        </div>
+        <!-- /.modal-content -->
+    </div>
+    <!-- /.modal-dialog -->
+</div>
+@php $u++; @endphp
+@endforeach
+
 @endsection
