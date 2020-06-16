6098b6f04f83e7e90e79967ac8156d778f675c8e
diah <www.diahapril@gmail.com>
2020-06-16 09:36:43 +0700

    web.php

--- routes/web.php
+++ routes/web.php
@@ -14,6 +14,7 @@ use Illuminate\Support\Facades\Route;
 
 
 Route::get('/', 'DataLongsorController@index');
+Route::get('/login', 'AuthController@login');
 Route::post('/longsor/tambah', 'DataLongsorController@tambah');
 Route::post('/longsor/edit/{id}', 'DataLongsorController@edit');
 Route::get('/longsor/hapus/{id}', 'DataLongsorController@hapus');
