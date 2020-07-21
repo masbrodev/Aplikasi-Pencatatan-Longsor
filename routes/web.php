880f4288f395439c8a05d64b35b95ed5f44816ab
Masbro <s2gotosingapore@gmail.com>
2020-07-21 13:49:22 +0700

    Add Route

--- routes/web.php
+++ routes/web.php
@@ -13,11 +13,14 @@ use Illuminate\Support\Facades\Route;
 */
 
 
-Route::get('/', 'DataLongsorController@index');
+Route::get('/longsor', 'DataLongsorController@index');
 Route::get('/login', 'AuthController@login');
 Route::post('/longsor/tambah', 'DataLongsorController@tambah');
 Route::post('/longsor/edit/{id}', 'DataLongsorController@edit');
 Route::get('/longsor/hapus/{id}', 'DataLongsorController@hapus');
 
 
-Route::get('/h', 'HomeController@index');
+Route::get('/', 'HomeController@index');
+Route::get('/kecamatan', 'KecamatanController@index');
+Route::get('/kerusakan', 'KerusakanController@index');
+Route::get('/peta', 'PetaController@index');
