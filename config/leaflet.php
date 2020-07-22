4ba5c0c111bd830fec11efb0ee8c39d949db4519
Masbro <s2gotosingapore@gmail.com>
2020-07-22 23:27:39 +0700

    Config location map

--- config/leaflet.php
+++ config/leaflet.php
@@ -1,8 +1,8 @@
 <?php
 
 return [
-    'zoom_level'           => 13,
+    'zoom_level'           => 11,
     'detail_zoom_level'    => 16,
-    'map_center_latitude'  => env('MAP_CENTER_LATITUDE', '-8.395636'),
-    'map_center_longitude' => env('MAP_CENTER_LONGITUDE', '115.175905'),
+    'map_center_latitude'  => env('MAP_CENTER_LATITUDE', '-7.737868042662'),
+    'map_center_longitude' => env('MAP_CENTER_LONGITUDE', '113.47864151000'),
 ];
