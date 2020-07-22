6ad8a147947d618f9775a16e26fd1757ba4ec9f5
Masbro <s2gotosingapore@gmail.com>
2020-07-23 03:29:47 +0700

    Config Kerusakan Controller

--- app/Http/Controllers/KerusakanController.php
+++ app/Http/Controllers/KerusakanController.php
@@ -3,11 +3,53 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Kerusakan;
 
 class KerusakanController extends Controller
 {
-    public function index()
+    public function index(Request $request)
     {
-        return view('pages.kerusakan');
+        $data['kerusakan'] = Kerusakan::get();
+        return view('pages.kerusakan', $data);
+        // return $data;
+    }
+
+    public function tambah(Request $request)
+    {
+        $data = [
+            'kecamatan' => $request->kecamatan,
+            'kelurahan' => $request->kelurahan,
+            'jenis_kerusakan' => $request->jenis_kerusakan,
+            'penyebab' => $request->penyebab,
+            'jumlah_kerusakan' => $request->jumlah_kerusakan,
+        ];
+
+        $simpan = Kerusakan::create($data);
+        if ($simpan) {
+            return redirect()->back();
+        }
+    }
+    public function edit(Request $request, $id)
+    {
+        $data = [
+            'kecamatan' => $request->kecamatan,
+            'kelurahan' => $request->kelurahan,
+            'jenis_kerusakan' => $request->jenis_kerusakan,
+            'penyebab' => $request->penyebab,
+            'jumlah_kerusakan' => $request->jumlah_kerusakan,
+        ];
+
+        $simpan = Kerusakan::where('id', $id)->update($data);
+        if ($simpan) {
+            return redirect()->back();
+        }
+    }
+
+    public function hapus($id)
+    {
+        $hapus = Kerusakan::where('id', $id)->delete();
+        if ($hapus) {
+            return redirect()->back();
+        }
     }
 }
