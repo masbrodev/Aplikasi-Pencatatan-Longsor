c06ab9ae130f86237b505202fb607f5a29e9adee
Masbro <s2gotosingapore@gmail.com>
2020-07-22 23:27:58 +0700

    Add script axios

--- resources/views/pages/peta.blade.php
+++ resources/views/pages/peta.blade.php
@@ -26,6 +26,8 @@
     integrity="sha512-/Nsx9X4HebavoBvEBuyp3I7od5tA0UzAxs+j83KgC8PU0kgB4XiK4Lfe4y4cgBtaRJQEIFCW+oC506aPT2L1zw=="
     crossorigin=""></script>
 
+    <script src="https://unpkg.com/axios/dist/axios.min.js"></script>
+
 <script>
     var map = L.map('mapid').setView([{{ config('leaflet.map_center_latitude') }}, {{ config('leaflet.map_center_longitude') }}], {{ config('leaflet.zoom_level') }});
     var baseUrl = "{{ url('/') }}";
