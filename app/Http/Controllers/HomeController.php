8b653cb5c61c0de8d368241e2393767165df97af
Masbro <s2gotosingapore@gmail.com>
2020-07-25 00:06:41 +0700

    Change Function

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -6,8 +6,13 @@ use Illuminate\Http\Request;
 
 class HomeController extends Controller
 {
-    public function index()
+    public function home()
     {
         return view('pages.dashboard');
+    }
+
+    public function index()
+    {
+        return view('welcome');
     }
 }
