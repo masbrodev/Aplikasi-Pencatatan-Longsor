ce3e4fa36e5ae2cd678d967f8acb30e643bf8ac6
Masbro <s2gotosingapore@gmail.com>
2020-07-24 22:30:29 +0700

    Fixing Pages Kerusakan

--- resources/views/pages/kerusakan.blade.php
+++ resources/views/pages/kerusakan.blade.php
@@ -40,7 +40,7 @@
                         <td>{{ $r->kelurahan }}</td>
                         <td>{{ $r->jumlah_kerusakan }}</td>
                         <td>
-                                <button type="button" class="btn btn-success" data-toggle="modal" data-target="#lihat-kerusakan-modal{{ $u }}">Lihat</button>
+                            <button type="button" class="btn btn-success" data-toggle="modal" data-target="#lihat-kerusakan-modal{{ $u }}">Lihat</button>
                         </td>
                     </tr>
                     @php $u++; @endphp
@@ -66,10 +66,11 @@
                 <form action="{{ URL::to('kerusakan/tambah')}}" method="post">
                     @csrf
 
-                    <label for="my-input">Kecamatan</label>
+                    <div class="form-group">
+                        <label for="my-input">Kecamatan</label>
                         <select name="kecamatan" class="form-control kecamatan" id="kecamatan">
-
                         </select>
+                    </div>
                     <div class="form-group">
                         <label>Kelurahan</label>
                         <select name="kelurahan" class="form-control kelurahan" id="kelurahan">
@@ -80,11 +81,11 @@
                         <label>Jenis Kerusakan</label>
                         <input type="text" class="form-control" placeholder="Masukkan Jenis Kerusakan" name="jenis_kerusakan">
                     </div>
-                     <div class="form-group">
+                    <div class="form-group">
                         <label>Penyebab</label>
                         <input type="text" class="form-control" placeholder="Masukkan Penyebab" name="penyebab">
                     </div>
-                     <div class="form-group">
+                    <div class="form-group">
                         <label>Jumlah Kerusakan</label>
                         <input type="text" class="form-control" placeholder="Masukkan Jumlah Kerusakan" name="jumlah_kerusakan">
                     </div>
@@ -118,25 +119,25 @@ $u = 1;
             <div class="modal-body">
                 <form action="{{ URL::to('kerusakan/edit/'.$r->id)}}" method="post">
                     @csrf
-
+                    <div class="form-group">
                         <label for="my-input">Kecamatan</label>
                         <select name="kecamatan" class="form-control kecamatan" id="kecamatan">
                         </select>
+                    </div>
                     <div class="form-group">
                         <label>Kelurahan</label>
                         <select name="kelurahan" class="form-control kelurahan" id="kelurahan">
                         </select>
-
                     </div>
                     <div class="form-group">
                         <label>Jenis Kerusakan</label>
                         <input type="text" class="form-control" placeholder="Masukkan Jenis Kerusakan" name="jenis_kerusakan" value="{{ $r->jenis_kerusakan}}">
                     </div>
-                     <div class="form-group">
+                    <div class="form-group">
                         <label>Penyebab</label>
                         <input type="text" class="form-control" placeholder="Masukkan Penyebab" name="penyebab" value="{{$r->penyebab}}">
                     </div>
-                     <div class="form-group">
+                    <div class="form-group">
                         <label>Jumlah Kerusakan</label>
                         <input type="text" class="form-control" placeholder="Masukkan Jumlah Kerusakan" name="jumlah_kerusakan" value="{{$r->jumlah_kerusakan}}">
                     </div>
@@ -172,40 +173,38 @@ $u = 1;
             </div>
             <div class="modal-body">
 
-                        <label for="my-input">Kecamatan</label>
-                        <select class="form-control" disabled="disabled">
-                        <option>{{ $r->kecamatan}}</option>
-                        </select>
-                    <div class="form-group">
-                        <label>Kecamatan</label>
-                        <select class="form-control" disabled="disabled">
+                <label for="my-input">Kecamatan</label>
+                <select class="form-control" disabled="disabled">
+                    <option>{{ $r->kecamatan}}</option>
+                </select>
+                <div class="form-group">
+                    <label>Kecamatan</label>
+                    <select class="form-control" disabled="disabled">
                         <option>{{ $r->kelurahan}}</option>
-                        </select>
+                    </select>
 
-                    </div>
-                    <div class="form-group">
-                        <label>Jenis Kerusakan</label>
-                        <input type="text" class="form-control" value="{{ $r->jenis_kerusakan}}" disabled>
-                    </div>
-                     <div class="form-group">
-                        <label>Penyebab</label>
-                        <input type="text" class="form-control" value="{{$r->penyebab}}" disabled>
-                    </div>
-                     <div class="form-group">
-                        <label>Jumlah Kerusakan</label>
-                        <input type="text" class="form-control"  value="{{$r->penyebab}}" disabled>
-                    </div>
+                </div>
+                <div class="form-group">
+                    <label>Jenis Kerusakan</label>
+                    <input type="text" class="form-control" value="{{ $r->jenis_kerusakan}}" disabled>
+                </div>
+                <div class="form-group">
+                    <label>Penyebab</label>
+                    <input type="text" class="form-control" value="{{$r->penyebab}}" disabled>
+                </div>
+                <div class="form-group">
+                    <label>Jumlah Kerusakan</label>
+                    <input type="text" class="form-control" value="{{$r->penyebab}}" disabled>
+                </div>
 
-                    <div class="modal-footer justify-content-between">
-                        <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+                <div class="modal-footer justify-content-between">
+                    <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
 
-                        <div class="btn-grub">
+                    <div class="btn-group">
                         <button type="button" class="btn btn-warning" data-dismiss="modal" data-toggle="modal" data-target="#edit-kerusakan-modal{{ $u }}">Edit</button>
                         <a href="{{ URL::to('kerusakan/hapus/'.$r->id) }}" class="btn btn-danger">Hapus</a>
-                        </div>
-                        
-
                     </div>
+                </div>
                 </form>
 
             </div>
@@ -220,11 +219,9 @@ $u = 1;
 
 
 @push('js')
-<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js"
-    integrity="sha512-bLT0Qm9VnAYZDflyKcBaQ2gg0hSYNQrJ8RilYldYQ1FxQYoCLtUjuuRuZo+fjqhx/qtq/1itJ0C2ejDxltZVFg=="
-    crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js" integrity="sha512-bLT0Qm9VnAYZDflyKcBaQ2gg0hSYNQrJ8RilYldYQ1FxQYoCLtUjuuRuZo+fjqhx/qtq/1itJ0C2ejDxltZVFg==" crossorigin="anonymous"></script>
 <script>
- api_provinsi();
+    api_provinsi();
 
     function api_provinsi() {
         var tag = '';
@@ -233,9 +230,9 @@ $u = 1;
             type: "GET",
             url: "https://dev.farizdotid.com/api/daerahindonesia/kecamatan?id_kota=3513",
             dataType: "JSON",
-            success: function (data) {
+            success: function(data) {
                 for (var index = 0; index < data['kecamatan'].length; index++) {
-                    tag += '<option value="' + data['kecamatan'][index].nama + '" myTag= "'+data['kecamatan'][index].id+'">' + data['kecamatan'][index].nama + '</option>';
+                    tag += '<option value="' + data['kecamatan'][index].nama + '" myTag= "' + data['kecamatan'][index].id + '">' + data['kecamatan'][index].nama + '</option>';
                     // id += [data['kecamatan'][index].id];
 
                     // console.log(id);
@@ -244,18 +241,18 @@ $u = 1;
                 $('.kecamatan').html(tag);
             }
         });
-        $('.kecamatan').click(function () {
+        $('.kecamatan').click(function() {
             var tag = '';
             id = $('#kecamatan option:selected').attr("myTag");
             // id = $('#kecamatan').val();
 
             // console.log(n);
-            
+
             $.ajax({
                 type: "GET",
                 url: "https://dev.farizdotid.com/api/daerahindonesia/kelurahan?id_kecamatan=" + id,
                 dataType: "JSON",
-                success: function (data) {
+                success: function(data) {
                     for (var index = 0; index < data['kelurahan'].length; index++) {
                         tag += '<option value="' + data['kelurahan'][index].nama + '">' + data['kelurahan'][index].nama + '</option>';
                         // id += [data['data'][index].id];
@@ -267,6 +264,5 @@ $u = 1;
             });
         })
     }
-
 </script>
 @endpush
