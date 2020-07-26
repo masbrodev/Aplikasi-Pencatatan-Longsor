eff30b009677c1c9609824901e208aae88de1411
Masbro <s2gotosingapore@gmail.com>
2020-07-26 13:24:30 +0700

    Fixing Welcome Pages

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -22,13 +22,42 @@
 
 <style>
     #mapid { min-height: 300px; }
+
+    .jumbotron {
+  position: relative;
+  overflow: hidden;
+  background-color:black;
+}
+.jumbotron .img {
+  position: absolute;
+  z-index: 1;
+  top: 0;
+  width:100%;
+  height:100%;
+  /*  object-fit is not supported on IE  */
+  object-fit: cover;
+  opacity:0.5;
+}
+
+.jumbotron .img2 {
+  position: relative;
+  z-index: 2;
+  top: 50%;
+  width:200px;
+  height:200px;
+}
+.jumbotron .container {
+  z-index: 2;
+  position: relative;
+}
+
 </style>
 </head>
 <body>
     <div id="app">
         <nav class="navbar navbar-expand-md navbar-light navbar-laravel">
             <div class="container">
-                <a class="navbar-brand" href="{{ url('/') }}">
+                <a class="navbar-brand" href="{{ url('/home') }}">
                     WebGIS
                 </a>
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
@@ -47,8 +76,23 @@
                 </div>
             </div>
         </nav>
-<br>
-<br>
+
+<div class="jumbotron jumbotron-fluid">
+
+<img class="img"src="https://images.pexels.com/photos/2291648/pexels-photo-2291648.jpeg?cs=srgb&dl=alam-asap-awan-berbayang-2291648.jpg&fm=jpg" alt="">
+<center>
+<img class="img2" src="{{ asset('logo.png')}}" alt="">
+</center>
+  <div class="container text-white ">
+
+    <h1 class="display-4">Aplikasi Webgis Pemetaan Longsor Kabupaten Probolinggo</h1>
+    <p class="lead">Hidup Bersinergi dengan Semesta Alam Kenali Bahayanya Kurangi Resikonya Probolinggo Tangguh Bencana Alam.</p>
+ </div>
+ <br>
+ <br>
+  <!-- /.container -->
+</div>
+<!-- /.jumbotron -->
         <main class="py-4 container">
         <div class="row">
           <div class="col-md-12">
@@ -56,7 +100,7 @@
               <div class="card-header">
                 <h3 class="card-title">
                 <center>
-                  Aplikasi Webgis Pemetaan Longsor Kabupaten Probolinggo
+                  Peta Sebaran Bencana Alam Kabupaten Probolinggo
                 </center>
                 </h3>
               </div>
@@ -70,9 +114,6 @@
             <!-- /.card -->
           </div>
           </div>
-
-            <div class="card">
-            </div>
         </main>
     </div>
     <!-- Scripts -->
@@ -81,9 +122,9 @@
 </body>
 </html>
 
-<!-- <script src="{{ asset('vendor/jquery/jquery.min.js') }}"></script>
-<script src="{{ asset('vendor/bootstrap/js/bootstrap.bundle.min.js') }}"></script>
-<script src="{{ asset('vendor/overlayScrollbars/js/jquery.overlayScrollbars.min.js') }}"></script> -->
+<script src="{{ asset('vendor/jquery/jquery.min.js') }}"></script>
+<!-- <script src="{{ asset('vendor/bootstrap/js/bootstrap.bundle.min.js') }}"></script> -->
+<!-- <script src="{{ asset('vendor/overlayScrollbars/js/jquery.overlayScrollbars.min.js') }}"></script> -->
 
 <script src="https://unpkg.com/leaflet@1.3.1/dist/leaflet.js"
     integrity="sha512-/Nsx9X4HebavoBvEBuyp3I7od5tA0UzAxs+j83KgC8PU0kgB4XiK4Lfe4y4cgBtaRJQEIFCW+oC506aPT2L1zw=="
@@ -92,6 +133,24 @@
     <script src="https://unpkg.com/axios/dist/axios.min.js"></script>
 
 <script>
+
+function deferVideo() {
+
+    //defer html5 video loading
+  $("video source").each(function() {
+    var sourceFile = $(this).attr("data-src");
+    $(this).attr("src", sourceFile);
+    var video = this.parentElement;
+    video.load();
+    // uncomment if video is not autoplay
+    video.play(); 
+  });
+
+}
+window.onload = deferVideo;
+
+
+
     var map = L.map('mapid').setView([{{ config('leaflet.map_center_latitude') }}, {{ config('leaflet.map_center_longitude') }}], {{ config('leaflet.zoom_level') }});
     var baseUrl = "{{ url('/') }}";
 
